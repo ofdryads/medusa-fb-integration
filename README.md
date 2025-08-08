a285b216f8bef7e9bc6a38dfa9ce8d016530944d
I <isabellehommel@outlook.com>
2025-08-08 14:11:43 -0400

    Scheduled product sync job

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# medusa-fish-glue
-This is an API integration between Medusa.js and {the ERP in question}
+# Medusa.js + Fishbowl ERP E-commerce integration
+This is an API integration between Medusa.js and FB inventory ERP software
+
+## It syncs:
+- Product info (FB -> Medusa)
+- Inventory count (FB -> Medusa)
+- Orders (Medusa -> FB)
+- Product categories added as custom fields in the ERP
---
+++ jobs/sync-products-job.ts
@@ -0,0 +1,22 @@
+import {
+  MedusaContainer,
+} from "@medusajs/framework/types"
+import { syncFromErpWorkflow } from "../workflows/sync-products-from-fb"
+import ErpModuleService from "../modules/erp/service"
+
+export default async function syncProductsJob(container: MedusaContainer) {
+  console.log("Syncing products...")
+  try {
+    const erpProducts = (await syncFromErpWorkflow(container).run({})).result.erpProducts
+    console.log(`Synced ${erpProducts.length} products`)
+  } finally {
+    const erpModuleService = container.resolve("erp") as ErpModuleService
+    await erpModuleService.logOutFb()
+  }
+}
+
+export const config = {
+  name: "daily-product-sync",
+  schedule: "0 0 * * *", // Every day at midnight
+  //schedule: "* * * * *", // Every minute
+}
