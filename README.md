9cf5375f70f60c6c219318c484ba2e74dddcda0e
I <71109631+ofdryads@users.noreply.github.com>
2025-10-06 14:00:22 -0400

    Refactor

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 # Medusa.js + Fishbowl ERP E-commerce integration
+
 API integration between Medusa.js and Fishbowl ERP software
 
 ## It syncs:
+
 - Product info (FB -> Medusa)
 - Inventory count (FB -> Medusa)
 - Orders (Medusa -> FB) (work in progress)
 
 ### Soon to be added
+
 - Product categories that have been added as custom fields in the ERP automatically sync and map to Medusa/website categories (product categories are not natively supported by this ERP)
 
 ## Dependencies/prerequisites:
+
 - Full Medusa.js app
 - Axios installed as a dependency in the Medusa project
 - Fishbowl server install, server URL and login credentials, and the Medusa app that will make requests manually approved in Fishbowl
@@ -23,17 +27,20 @@ Fishbowl API docs example they have online: https://help.fishbowlinventory.com/a
 More up to date Fishbowl API docs: `{SERVER_URL}:2456/apidocs`
 
 ## Jobs/Tasks
+
 - Website Order-to-ERP task is a "subscriber" - it listens for the "order placed" event then runs
-- ERP Inventory Counts-to-Website and ERP Products-to-Website are scheduled jobs: 
+- ERP Inventory Counts-to-Website and ERP Products-to-Website are scheduled jobs:
   - Product runs every 24 hours
   - Inventory runs every 3 hours
 
 ## Auth
+
 - Fishbowl does API auth as if it were a user logging into their client software
 - Token is granted in exchange for valid user login using /api/login endpoint
 - Must provide a username, password, the name of the app using the API, and the app's assigned ID number that's registered in Fishbowl. These values should be environment variables.
 
 ### Example of how environment variables for Fishbowl go in medusa-config.ts
+
 ```
 module.exports = defineConfig({
   projectConfig: {
@@ -56,10 +63,11 @@ module.exports = defineConfig({
 ```
 
 ## Notes on constraints
+
 - None of the api/sales-orders endpoints actually allow you to create an order in FB that does not yet exist (i.e. one just placed on the website)
-    - So as of right now summer 2025, have to use POST /api/import/Sales-Order (the CSV method) for this
+  - So as of right now summer 2025, have to use POST /api/import/Sales-Order (the CSV method) for this
 - The api/export CSV endpoints do not accept query parameters - it's just a CSV dump that would need to be converted to actual JSON from a 2d array and then filtered/transformed extensively after payload is received, using our compute not FB server's. # of rows for product/part are in the tens of thousands.
 - I am begrudgingly using their endpoint that takes raw SQL as a query parameter for product/inventory sync to circumvent that. The queries are hardcoded on the Medusa backend & no user input which minimizes risk
-- Fishbowl licensing comes with a concurrent user login limit per license - any API call made when the logged-in user limit is already at max will fail 
+- Fishbowl licensing comes with a concurrent user login limit per license - any API call made when the logged-in user limit is already at max will fail
 - Related to above, a task using the API must log out promptly after its job has completed (or failed) so that actual people working will not be blocked from logging into the client software (see below section)
 - For those reasons, if any other scheduled tasks are added, they should run outside of business hours if possible
--- modules/erp/service.ts
+++ modules/erp/service.ts
@@ -1,40 +1,40 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance } from "axios";
 
 type Options = {
-  username: string
-  password: string
-  appName: string
-  appId: number
-  fbUrl: string
-}
+  username: string;
+  password: string;
+  appName: string;
+  appId: number;
+  fbUrl: string;
+};
 
 export type ErpProduct = {
-  customFields: string 
-  price: number
-  num: string
-  description: string
-  weight: number
-}
+  customFields: string;
+  price: number;
+  num: string;
+  description: string;
+  weight: number;
+};
 
 export type ErpQuantity = {
-  num: string
-  total_qty: number
-}
+  num: string;
+  total_qty: number;
+};
 
-type InjectedDependencies = {}
+type InjectedDependencies = {};
 
 export default class ErpModuleService {
-  private options: Options
-  private fbClient: AxiosInstance
-  private token: string | null = null
+  private options: Options;
+  private fbClient: AxiosInstance;
+  private token: string | null = null;
 
   constructor(_: InjectedDependencies, options: Options) {
-    this.options = options
+    this.options = options;
     // initialize client that connects to ERP
     this.fbClient = axios.create({
       baseURL: options.fbUrl,
-      timeout: 10_000
-    })
+      timeout: 10_000,
+    });
   }
 
   private async auth(): Promise<void> {
@@ -46,7 +46,7 @@ export default class ErpModuleService {
 
         //auth details for app (Medusa)
         appId: this.options.appId,
-        appName: this.options.appName
+        appName: this.options.appName,
       });
 
       if (!res.data?.token) {
@@ -55,55 +55,40 @@ export default class ErpModuleService {
 
       this.token = res.data.token;
     } catch (error) {
-      console.error("Authentication error:", error)
+      console.error("Authentication error:", error);
       throw error;
     }
   }
 
   private getToken(): string {
     if (!this.token) {
-      throw new Error("Token not available")
+      throw new Error("Token not available");
     }
-    return this.token
+    return this.token;
   }
 
   async logOutFb() {
     try {
-      if (!this.token) return
+      if (!this.token) return;
       await this.fbClient.post("/api/logout", null, {
-        headers: { "Authorization": `Bearer ${this.getToken()}` }
-      })
+        headers: { Authorization: `Bearer ${this.getToken()}` },
+      });
     } catch (error) {
-      console.error("Failed to log out of Fishbowl:", error)
-      //don't throw error, this is not a fatal error
-    } finally { this.token = null }
-  }
-
-  // reusable for GET requests to /api/data-query endpoint
-  async dataQueryGetReq(query: string) {
-    const encodedQuery = encodeURIComponent(query)
-
-    const res = await this.fbClient.get(`/api/data-query?query=${encodedQuery}`, {
-      headers: {
-        "Authorization": `Bearer ${this.getToken()}`,
-        "Accept": "application/json"
-      }
-    })
-
-    return res.data
+      //don't throw, this is not a fatal error
+      console.error("Failed to log out of Fishbowl:", error);
+    } finally {
+      this.token = null;
+    }
   }
 
   //get fishbowl product listings
   async getProducts() {
     try {
-      if (!this.token) await this.auth()
+      if (!this.token) await this.auth();
 
       /* adjust SELECT fields as more info is added to website and fishbowl
-       before filtering based on exclusion category, first filter for only products with non-zero quantity and price
-       NOT EXISTS prevents products categorized as not belonging on the website from syncing
-       Check if exclusion "product tree" category (it's called "Ignore") still exists before filtering
-       *needs indexing 
-      */
+       * filters for only products with non-zero quantity and price
+       */
       const query = `
         SELECT num, description, weight, price, customFields
         FROM product pr
@@ -116,20 +101,29 @@ export default class ErpModuleService {
             WHERE pt.num = pr.num
             AND pc.qty > 0
           );
-        `
-      const data = await this.dataQueryGetReq(query) //make the GET request to Fishbowl with above query
-      return data
+        `;
+      const encodedQuery = encodeURIComponent(query);
+      const res = await this.fbClient.get(
+        `/api/data-query?query=${encodedQuery}`,
+        {
+          headers: {
+            Authorization: `Bearer ${this.getToken()}`,
+            Accept: "application/json",
+          },
+        }
+      );
 
+      return res.data;
     } catch (error) {
-      console.error("Error getting product data:", error)
-      throw error
+      console.error("Error getting product data:", error);
+      throw error;
     }
   }
 
   // get part/product number and its quantity
   async getInventoryQuantities() {
     try {
-      if (!this.token) await this.auth()
+      if (!this.token) await this.auth();
 
       /* this links Fishbowl's part ID to the part number, then links part # to product #
         Only retrieve inventory w/ non-zero quantities. If inventory currently listed on the website as in-stock is not returned by this query, it is assumed to have gone out of stock
@@ -141,65 +135,35 @@ export default class ErpModuleService {
         JOIN product pr ON pr.num = p.num
         WHERE pc.qty > 0
         GROUP BY pr.num, pc.partId;
-      `
-      
-      const data = await this.dataQueryGetReq(query)
-      return data
+      `;
+      const encodedQuery = encodeURIComponent(query);
+      const res = await this.fbClient.get(
+        `/api/data-query?query=${encodedQuery}`,
+        {
+          headers: {
+            Authorization: `Bearer ${this.getToken()}`,
+            Accept: "application/json",
+          },
+        }
+      );
 
+      return res.data;
     } catch (error) {
-      console.error("Error getting inventory quantities:", error)
-      throw error
+      console.error("Error getting inventory quantities:", error);
+      throw error;
     }
   }
 
-  // DRAFT FUNCTION
   async sendOrderToErp(order) {
     try {
-      if (!this.token) await this.auth()
+      if (!this.token) await this.auth();
 
       const res = await this.fbClient.post("/api/import/Sales-Order", {
         // order
-      })
+      });
     } catch (error) {
       console.error("Error sending order to Fishbowl: ", error);
-      throw error
+      throw error;
     }
   }
-
-  async deleteOrder(order) {
-    // delete order
-  }
 }
-
-/* Below is a reference of the format in which order data will need to be sent to the ERP
-it is CSV Data / 2D array 
-the values from [3] and beyond have product data corresponding to headers in [1]
-[2] data corresponds to [0] headers
-
-It needs a ton of mapping and processing to become like this and not trip Fishbowl's CSV data validation
-*/
-
-/*[
-  [
-    "Flag", "SONum", "Status", "CustomerName", "CustomerContact", "BillToName", "BillToAddress",
-    "BillToCity", "BillToState", "BillToZip", "BillToCountry", "ShipToName", "ShipToAddress",
-    "ShipToCity", "ShipToState", "ShipToZip", "ShipToCountry", "ShipToResidential", "CarrierName",
-    "TaxRateName", "PriorityId", "Date", "CarrierService", "Phone", "Email"
-  ],
-  [
-    "Flag", "SOItemTypeID", "ProductNumber", "ProductQuantity", "UOM", "ProductPrice", "Taxable", "TaxCode", "Note",
-    "ItemDateScheduled"
-  ],
-  [
-    //customer data - (EXAMPLE, FAKE)
-     "SO", "10783", "10", "Canyon Outfitters", "Firstname Lastname", "Canyon Outfitters", "982 River Rd",
-"Boise", "ID", "83702", "UNITED STATES", "Canyon Outfitters", "982 River Rd", "Boise",
-"ID", "83702", "UNITED STATES", "false", "UPS", "Cal Tax", "30", "06/28/2025", "Ground", "208-555-9981", "liam@canyonoutfitters.com", "Express"
-  ],
-  [
-    // product1
-  ],
-  [ //product2 etc....
-  ],
-]
-*/
